a273e3cb352dc04bfdc5ef4001db83d39a8546e5
toho36 <toho0036@gmail.com>
2024-12-18 18:22:56 +0100

    adding routes

--- apps/server/app.ts
+++ apps/server/app.ts
@@ -1,12 +1,14 @@
 import { Hono } from 'hono';
 import { logger } from 'hono/logger';
-
+import { expensesRoute } from './routes/expenses';
 const app = new Hono();
 
-app.use(logger());
+app.use('*', logger());
 
 app.get('/test', (c) => {
   return c.json({ message: 'server is running' });
 });
+
+app.route('/api/expenses', expensesRoute);
 
 export default app;
