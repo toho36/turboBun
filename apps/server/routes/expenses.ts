25d999474946516f82e23965a89750247956d36a
toho36 <toho0036@gmail.com>
2024-12-19 12:18:33 +0100

    Zod validation

--- apps/server/routes/expenses.ts
+++ apps/server/routes/expenses.ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono';
+import { z } from 'zod';
 
 type Expense = {
   id: number;
@@ -10,12 +11,19 @@ const fakeExpenses: Expense[] = [
   { id: 2, title: 'Lunch', amount: 12.0 },
   { id: 3, title: 'Groceries', amount: 45.0 },
 ];
+
+const createPostSchema = z.object({
+  title: z.string().min(3).max(100),
+  amount: z.number().int().positive(),
+});
+
 export const expensesRoute = new Hono()
   .get('/', async (c) => {
-    return c.json({ expenses: [] });
+    return c.json({ expenses: fakeExpenses });
   })
   .post('/', async (c) => {
-    const expense = await c.req.json();
+    const data = await c.req.json();
+    const expense = createPostSchema.parse(data);
     console.log({ expense });
     return c.json(expense);
   });
