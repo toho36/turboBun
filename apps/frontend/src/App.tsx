04c0d711db3153bbf7753fe97f07babc9533eee3
toho36 <toho0036@gmail.com>
2025-01-03 16:38:56 +0100

    setup Shadcn
    installing shadcn and adding @ to src directory when importing

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,23 +1,13 @@
 import { useState } from 'react';
-
+import { Button } from '@/components/ui/button';
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className="flex flex-col">
-        <button
-          className="bg-blue-500"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Up
-        </button>
-        <button
-          className="bg-red-100"
-          onClick={() => setCount((count) => count - 1)}
-        >
-          Downs
-        </button>
+      <div className="flex flex-col bg-background max-w-md m-auto gap-y-5">
+        <Button onClick={() => setCount((count) => count + 1)}>Up</Button>
+        <Button onClick={() => setCount((count) => count - 1)}>Downs</Button>
         <div className="text-center">{count}</div>
       </div>
     </>
